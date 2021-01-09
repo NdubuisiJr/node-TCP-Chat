83550cd76d96b688b8f6c3b9b41f1dd5cf0ac13e
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-01-09 21:20:15 +0100

    added comments to the client app

--- src/client.js
+++ src/client.js
@@ -1,17 +1,27 @@
+/**
+ *  The TCP Chat Client Implementation
+ */
+
 const net = require('net');
 const stdin = process.stdin;
 
+// connect to the chat server using the host name and port.
 const socket = net.connect(3000,'localhost',()=>{
     console.log('connected!');
 });
+
+// encoding for better character reading
 socket.setEncoding('utf8');
 
+// write received messages to the console (standout out)
 socket.on('data',(data)=>{
     process.stdout.write(data);
 });
 
+// read input from the keyboard (standard in)
 stdin.resume();
 stdin.setEncoding('utf8');
 stdin.on('data', data =>{
+    // write/send input from keyboard to the server.
     socket.write(data+'\n\r');
 });
